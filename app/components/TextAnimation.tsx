a3a6b1410c615cb234dd39d70300306b2a75a024
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-07-14 22:52:32 +0100

    upating

--- app/components/TextAnimation.tsx
+++ app/components/TextAnimation.tsx
@@ -15,56 +15,27 @@ export default function TextAnimation({
       whileHover="hovered"
       className="overflow-hidden relative"
     >
-      <div>
+      <motion.div
+        variants={{
+          initial: {
+            y: "0",
+            opacity: 0,
+          },
+          hovered: {
+            y: "-100%",
+            opacity: 0,
+          },
+        }}
+        transition={{
+          duration: 0.25,
+          ease: "easeInOut",
+          delay: 0.025,
+        }}
+      >
         {title.split("").map((l, i) => (
-          <motion.span
-            variants={{
-              initial: {
-                y: "0",
-                opacity: 0,
-              },
-              hovered: {
-                y: "-100%",
-                opacity: 0,
-              },
-            }}
-            transition={{
-              duration: 0.25,
-              ease: "easeInOut",
-              delay: i * 0.025,
-            }}
-            className="inline-block"
-            key={i}
-          >
-            {l}
-          </motion.span>
+          <span key={i}>{l}</span>
         ))}
-      </div>
-      <div className=" absolute inset-0">
-        {title.split("").map((l, i) => (
-          <motion.span
-            variants={{
-              initial: {
-                y: "100%",
-                opacity: 0,
-              },
-              hovered: {
-                y: 0,
-                opacity: 1,
-              },
-            }}
-            transition={{
-              duration: 0.25,
-              ease: "easeInOut",
-              delay: i * 0.025,
-            }}
-            className="inline-block"
-            key={i}
-          >
-            {l}
-          </motion.span>
-        ))}
-      </div>
+      </motion.div>
     </motion.div>
   );
 }
