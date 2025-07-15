7d0097e86c21c4f2610c33292149df363657f887
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-07-15 15:36:55 +0100

    Hero section

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import TextAnimation from "./TextAnimation";
 export default function Header() {
   return (
-    <header className="flex sticky top-0 left-0 font-semibold p-5  justify-between items-center ">
+    <header className="flex h-[10vh] sticky top-0 left-0 font-semibold p-5 bg-whit/40  backdrop-blur-2xl  justify-between items-center ">
       <h1 className="text-xl">Theo Markin</h1>
       <div className="flex gap-4 items-center">
         <Link href={"work"} className="py-1 px-3 rounded-md hover:bg-[#e3f794]">
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,3 +1,42 @@
+import Image from "next/image";
+import TextAnimation from "./TextAnimation";
+
 export default function Hero() {
-  return <div className="h-[600vh] bg-red-800"></div>;
+  return (
+    <div className="h-[90vh] p-20 flex flex-col items-start gap-20 justify-end">
+      <div>
+        <Image
+          src={
+            "https://framerusercontent.com/images/0yOcRB6WzTI3QPLHaLLEeX8Hp6Q.svg"
+          }
+          width={100}
+          height={100}
+          alt="Logo"
+        />
+        <h2 className="text-4xl mt-10 w-1/2">
+          Bristol based digital designer crafting engaging, human-focused
+          digital experiences for a range of forward-thinking brands.
+        </h2>
+      </div>
+      <div className="flex items-center justify-between  w-full opacity-60">
+        <div>
+          <h3>Freelance</h3>
+          <p>Digital Designer</p>
+        </div>
+        <div>
+          <h3>Based in</h3>
+          <p>Bristol</p>
+        </div>
+        <div>
+          <h3>Get in touch</h3>
+          <span>
+            <TextAnimation
+              title="hello@joshwhite.design"
+              semiTitle="hello@joshwhite.design"
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
