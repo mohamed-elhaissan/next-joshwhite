a69fb807808f966788b59e16278646da0f6e20de
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-07-15 17:42:35 +0100

    updating

---
+++ app/components/Info.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+export default function Info(){
+    return(
+        <div>
+            Info ?
+        </div>
+    )
+}
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -8,7 +8,7 @@ import card4 from "../../public/projects/card-01.avif";
 import card5 from "../../public/projects/s6jQbCVk3i01aWALA8FM4Ax70fA.webp";
 import card6 from "../../public/projects/xcn8qNpAaTFuCmLRCApLCh0zc.webp";
 import { useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { StaticImageData } from "next/image";
 
@@ -17,18 +17,26 @@ export default function Projects() {
   const { scrollYProgress } = useScroll({
     target: targetContainer,
   });
-  const x: any = useTransform(scrollYProgress, [0, 1], ["1%", "-100%"]);
-  const images: StaticImageData[] = [card4, card6, card3, card5, card1, card2];
+  const x: any = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
+  const images: StaticImageData[] = [card4, card6, card5, card3, card1, card2];
+  const titles: string[] = [
+    "Lyle & Scott",
+    "B!POD",
+    "Db Journey",
+    "EyeWear by David",
+    "Meermin",
+    "Dockers",
+  ];
   return (
     <div
       ref={targetContainer}
       className=" text-white  h-[300vh] bg-[var(--backgroundBlack)]"
     >
-      <div className="sticky top-0 p-20 left-0 w-full flex   overflow-hidden h-screen  justify-center flex-col items-start">
-        <h1 className="text-7xl p-20">Projects</h1>
+      <div className="sticky top-0 p-10 left-0 w-full flex   overflow-hidden h-screen  justify-start flex-col items-start">
+        <h1 className="text-7xl py-20">Projects</h1>
         <motion.div style={{ x }} className="flex  w-full  gap-10">
           {images.map((item: StaticImageData, index: number) => (
-            <Card title="title" imageData={item} key={index} />
+            <Card title={titles[index]} imageData={item} key={index} />
           ))}
         </motion.div>
       </div>
@@ -43,16 +51,39 @@ const Card = ({
   imageData: StaticImageData;
   title: string;
 }) => {
+  const [itemHovered, setItemHovered] = useState<boolean>(false);
   return (
-    <div className="min-w-[500px]">
+    <div
+      onMouseEnter={() => {
+        setItemHovered(true);
+      }}
+      onMouseLeave={() => setItemHovered(false)}
+      className="min-w-[500px] cursor-pointer"
+    >
       <Image
         src={imageData}
         alt={`${title}`}
         width={550}
         height={400}
-        className="rounded-lg  object-cover "
+        className="rounded-lg    object-cover "
       />
-      <span>{title}</span>
+
+      <motion.span
+        initial={{
+          y: -10,
+          opacity: 0,
+        }}
+        animate={{
+          y: itemHovered ? 0 : -10,
+          opacity: itemHovered ? 1 : 0,
+        }}
+        transition={{
+          ease: "easeInOut",
+        }}
+        className="inline-block mt-3 text-[#e3f794]"
+      >
+        {title}
+      </motion.span>
     </div>
   );
 };
